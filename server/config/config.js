e31a344abeb7cda8823cff51e21f3ba920b370a0
Cristian Giraldo <ccgiraldo2@gmail.com>
2020-09-26 02:13:14 -0500

    Variable de entorno de HEROKU

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://TheThian:dfolIvnL35K0w00a@cluster0.v8iug.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 // Crear variables de enterno
